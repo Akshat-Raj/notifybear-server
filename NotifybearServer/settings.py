9f75b521bb9a3b0fbfb81ea7ede33250f2fa164f
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-11-06 22:01:39 +0530

    Set user model

--- NotifybearServer/settings.py
+++ NotifybearServer/settings.py
@@ -30,6 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".pythonanywhere.com"]
 
+AUTH_USER_MODEL = 'Accounts.User'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'NotifybearServer',
     'Accounts',
     'Notifications',
 ]
