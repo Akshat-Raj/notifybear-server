b8ff3e66626f45426fa8bd0ea002b378535e98a9
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-11-07 00:06:28 +0530

    Added serializers for APIs

---
+++ Accounts/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from .models import User, UserProfile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    dp_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = UserProfile
+        fields = ["dp", "dp_url"]
+
+    def get_dp_url(self, obj):
+        if obj.dp and hasattr(obj.dp, 'url'):
+            return obj.dp.url
+        return None
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(read_only=True)
+    initials = serializers.ReadOnlyField()
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "initials",
+            "profile",
+        ]
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "first_name", "last_name", "password"]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        # create UserProfile automatically
+        UserProfile.objects.create(user=user)
+
+        return user
---
+++ Notifications/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Notifications, NotificationMessage
+
+
+class NotificationMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NotificationMessage
+        fields = ["id", "sender", "message_text", "message_time"]
+
+class NotificationsSerializer(serializers.ModelSerializer):
+    messages = NotificationMessageSerializer(many=True, read_only=True)
+
+    reaction_time = serializers.DurationField(read_only=True)
+
+    class Meta:
+        model = Notifications
+        fields = [
+            "id",
+            "user",
+            "package_name",
+            "channel_id",
+            "post_time",
+            "timestamp_opened",
+            "reaction_time",
+            "title",
+            "text",
+            "big_text",
+            "sub_text",
+            "summary_text",
+            "info_text",
+            "text_lines",
+            "large_icon_base64",
+            "picture_base64",
+            "conversation_title",
+            "people",
+            "created_at",
+            "messages",
+        ]
+        read_only_fields = ("user", "created_at", "reaction_time")
